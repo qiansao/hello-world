f34a8ad9eda303aaaac2ffa31ef05f7537d6a9c3
qiansao <32014505+qiansao@users.noreply.github.com>
2019-10-15 09:15:31 +0800

    Update hello.c

--- hello.c
+++ hello.c
@@ -5,16 +5,15 @@ int main(void)
 {
   CURL *curl ;
   CURLcode res;
-  
-  curl=curl_ easy_ init();
+
+  curl=curl_easy_init();
   if(curl){
-  
-curl_ easy_ _setopt(curl, CURLOPT_URL, "http://myip. ipip.net/");
-    res=curl_ easy_ perform(curl);
-    if(res!=CURLE_ OK)
-    fprintf(stderr,"curl_easy_perform() failed:%s\n",curl_easy_strerror(res));
-    curl_easy_cleanup(curl);
-}
+    curl_easy_setopt(curl, CURLOPT_URL, "http://myip.ipip.net/");
+    res=curl_easy_perform(curl);
+    if(res!=CURLE_OK)
+      fprintf (stderr,"curl_easy_perform() failed:%s\n",
+              curl_easy_strerror(res));
+      curl_easy_cleanup(curl);
+  }
 return 0;
 }
-
